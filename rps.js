dec8d7892a25d65432ebc42a929da726de240fd5
Ughhhjake <ughhhjake@gmail.com>
2022-10-26 22:37:46 -0500

    it keeps score and declares a winner

--- rps.js
+++ rps.js
@@ -1,4 +1,3 @@
-
 function getPlayerChoice() {
   let randomWord = prompt("What is your selection *cap first letter no punctuation*");
   const playerWord = randomWord.toLowerCase();
@@ -21,7 +20,7 @@ function getPlayerChoice() {
 
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3);
-  console.log(randomNumber);
+  //console.log(randomNumber);
    if (randomNumber == 0) {
      console.log("Paper")
      return "Paper"
@@ -34,49 +33,55 @@ function getComputerChoice() {
    }
 }
 
-
-//let playerSelection = getPlayerChoice();
-//console.log(playerSelection + " this is player select");
-//let computerSelection = getComputerChoice();
-//console.log(computerSelection + " this is computer select");
-
-
-
 function playRound(playerSelection, computerSelection) {
-
 
   if (playerSelection == "Rock" && computerSelection == "Rock" ||
       playerSelection == "Scissors" && computerSelection == "Scissors" ||
       playerSelection == "Paper" && computerSelection == "Paper") {
     console.log("Tie, unlucky fam.");
-    return "unlucky brother";
+    return "tie";
 
   } else if (playerSelection == "Rock" && computerSelection == "Paper" ||
              playerSelection == "Paper" && computerSelection == "Scissors" ||
              playerSelection == "Scissors" && computerSelection == "Rock") {
     console.log("Computer Wins. " + computerSelection + " beats " + playerSelection);
-    return "computer dub";
-
+  //  computerScore++;
+    return "computer";
 
   } else if (playerSelection == "Rock" && computerSelection == "Scissors" ||
              playerSelection == "Paper" && computerSelection == "Rock" ||
              playerSelection == "Scissors" && computerSelection == "Paper") {
     console.log("Player Wins. " + playerSelection + " beats " + computerSelection);
-    return "player dub";
-
+  //  playerScore++;
+    return "player";
   }
 }
 
 function game() {
+  let computerScore = 0;
+  let playerScore = 0;
+
   for (let i = 0; i < 5; i++) {
    let playerSelection = getPlayerChoice();
    let computerSelection = getComputerChoice();
-   playRound(playerSelection, computerSelection);
-   console.log("---------")
+   let winner = playRound(playerSelection, computerSelection);
+        if (winner == "player") {
+          playerScore++;
+      } if (winner == "computer") {
+          computerScore++;
+      }
+      console.log(playerScore + " player wins and " + computerScore + " computer wins.")
+      console.log("---------")
+
   }
+     if (playerScore > computerScore) {
+       console.log("Player has won!")
+     } if (computerScore > playerScore) {
+       console.log("Computer has won.")
+     } if (playerScore = computerScore) {
+       console.log("That's a draw!")
+     }
 }
 
 game();
 console.log("Game Over.")
-
-//console.log(playRound(playerSelection, computerSelection));
