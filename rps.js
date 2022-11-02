b1c4464980aa6eb7d801a9c269646db2c185d962
Ughhhjake <ughhhjake@gmail.com>
2022-11-02 17:41:36 -0500

    does some stuff

--- rps.js
+++ rps.js
@@ -1,86 +1,87 @@
-function getPlayerChoice() {
-  let randomWord = prompt("Rock, Paper, or Scissors");
-  const playerWord = randomWord.toLowerCase();
+let choice = 0;
+let computerSelection = 0;
 
-  if (playerWord == "rock") {
-  console.log("Player has selected Rock.")
-  return "Rock"
-} else if (playerWord == "paper") {
-  console.log("Player has selected Paper.")
-  return "Paper"
-} else if (playerWord == "scissors") {
-  console.log("Player has selected Scissors.")
-  return "Scissors"
-} else {
-  alert("Bruh, L selection.")
-  console.log("Bro, thats not an option.")
-}
-}
-
-
-function getComputerChoice() {
+const btnRock = document.querySelector('#btnR');
+btnRock.addEventListener('click', () => {
+  let choice = "Rock";
+  console.log(choice);
+  console.log("Player has selected Rock");
   let randomNumber = Math.floor(Math.random() * 3);
   //console.log(randomNumber);
    if (randomNumber == 0) {
-     console.log("Paper")
-     return "Paper"
+     let computerSelection = "Paper";
+     console.log(computerSelection + "computerSelection");
    } else if (randomNumber == 1) {
-     console.log("Rock")
-     return "Rock"
+     let computerSelection = "Rock";
+     console.log(computerSelection + "computerSelection");
    } else if (randomNumber == 2) {
-     console.log("Scissors")
-     return "Scissors"
+     let computerSelection = "Scissors";
+     console.log(computerSelection + "computerSelection");
+   }  
+});
+
+const btnPaper = document.querySelector('#btnP');
+btnPaper.addEventListener('click', () => {
+  let choice = "Paper";
+  console.log(choice);
+  console.log("Player has selected Paper");
+  let randomNumber = Math.floor(Math.random() * 3);
+  //console.log(randomNumber);
+   if (randomNumber == 0) {
+     let computerSelection = "Paper";
+     console.log(computerSelection + "computerSelection");
+   } else if (randomNumber == 1) {
+     let computerSelection = "Rock";
+     console.log(computerSelection + "computerSelection");
+   } else if (randomNumber == 2) {
+     let computerSelection = "Scissors";
+     console.log(computerSelection + "computerSelection");
    }
-}
+});
 
-function playRound(playerSelection, computerSelection) {
 
-  if (playerSelection == "Rock" && computerSelection == "Rock" ||
-      playerSelection == "Scissors" && computerSelection == "Scissors" ||
-      playerSelection == "Paper" && computerSelection == "Paper") {
+const btnScissors = document.querySelector('#btnS');
+btnScissors.addEventListener('click', () => {
+  let choice = "Scissors";
+  console.log(choice);
+  console.log("Player has selected Scissors");
+  let randomNumber = Math.floor(Math.random() * 3);
+  //console.log(randomNumber);
+   if (randomNumber == 0) {
+     let computerSelection = "Paper"
+     console.log(computerSelection + "computerSelection");
+   } else if (randomNumber == 1) {
+     let computerSelection = "Rock"
+     console.log(computerSelection + "computerSelection");
+   } else if (randomNumber == 2) {
+     let computerSelection = "Scissors"
+     console.log(computerSelection + "computerSelection");
+   }
+});
+
+
+function playRound(choice, computerSelection) {
+
+  if (choice == "Rock" && computerSelection == "Rock" ||
+      choice == "Scissors" && computerSelection == "Scissors" ||
+      choice == "Paper" && computerSelection == "Paper") {
     console.log("Tie, unlucky fam.");
     return "tie";
 
-  } else if (playerSelection == "Rock" && computerSelection == "Paper" ||
-             playerSelection == "Paper" && computerSelection == "Scissors" ||
-             playerSelection == "Scissors" && computerSelection == "Rock") {
+  } else if (choice == "Rock" && computerSelection == "Paper" ||
+             choice == "Paper" && computerSelection == "Scissors" ||
+             choice == "Scissors" && computerSelection == "Rock") {
     console.log("Computer Wins. " + computerSelection + " beats " + playerSelection);
   //  computerScore++;
     return "computer";
 
-  } else if (playerSelection == "Rock" && computerSelection == "Scissors" ||
-             playerSelection == "Paper" && computerSelection == "Rock" ||
-             playerSelection == "Scissors" && computerSelection == "Paper") {
-    console.log("Player Wins. " + playerSelection + " beats " + computerSelection);
+  } else if (choice == "Rock" && computerSelection == "Scissors" ||
+             choice == "Paper" && computerSelection == "Rock" ||
+             choice == "Scissors" && computerSelection == "Paper") {
+    console.log("Player Wins. " + choice + " beats " + computerSelection);
   //  playerScore++;
     return "player";
   }
-}
-
-function game() {
-  let computerScore = 0;
-  let playerScore = 0;
-
-  for (let i = 0; i < 5; i++) {
-   let playerSelection = getPlayerChoice();
-   let computerSelection = getComputerChoice();
-   let winner = playRound(playerSelection, computerSelection);
-        if (winner == "player") {
-          playerScore++;
-      } if (winner == "computer") {
-          computerScore++;
-      }
-      console.log(playerScore + " player wins and " + computerScore + " computer wins.")
-      console.log("---------")
-
-  }
-     if (playerScore > computerScore) {
-       console.log("Player has won!")
-     } if (computerScore > playerScore) {
-       console.log("Computer has won.")
-     } if (playerScore == computerScore) {
-       console.log("That's a draw!")
-     }
 }
 
 
